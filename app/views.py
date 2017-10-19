4d254a156d13655c776bf1dbd35f7a3a82d27f8e
Granton Mchana <gmchana254@gmail.com>
2017-10-19 14:01:29 +0300

    additions

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Home - Welcome to Wananchi News Review Web'
+    return render_template('index.html', title = title)
+
+
+@app.route('/article/<int:article_id>')
+def article(article_id):
+
+    '''
+    View article page function that returns the article details page and its data
+    '''
+    return render_template('article.html',id = article_id)
