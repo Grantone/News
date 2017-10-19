daf89489d2250174835de6e58b3ced3c5a4395e0
Granton Mchana <gmchana254@gmail.com>
2017-10-19 16:09:58 +0300

    insertions

--- app/request.py
+++ app/request.py
@@ -32,3 +32,54 @@ def get_source():
 
 
     return source_results
+
+
+
+    # Getting the article base url
+    base_url = app.config['ARTICLE_API_BASE_URL']
+
+def get_article():
+    '''
+    Function that gets the json response to our urlrequest
+    '''
+
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_url_data = url.read()
+        get_articles_response =json.loads(get_articless_data)
+
+        article_results = None
+
+        if get_articles_response['articles']:
+            article_results_list =get_results_response['articles']
+            article_results =process_articles(article_results_list)
+
+
+    return article_results
+
+
+
+
+
+def process_results(source_list):
+    '''
+    Function  that processes the source result and transform them to a list of Objects
+
+    Args:
+        source_list: A list of dictionaries that contain source details
+
+    Returns :
+        source_results: A list of source objects
+    '''
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('original_name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+
+        if name:
+            source_object = Source(id,name,description,url,category)
+            source_results.append(source_object)
+
+    return source_results
