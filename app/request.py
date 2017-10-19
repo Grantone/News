87dcb83b408441ff07363a8d0841b822c43988bd
Granton Mchana <gmchana254@gmail.com>
2017-10-19 15:39:14 +0300

    additions

--- app/request.py
+++ app/request.py
@@ -8,8 +8,27 @@ Source = source.Source
 
 
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
+# api_key = app.config['NEWS_API_KEY']
 
 
-# Getting the news base url
-base_url = app.config["NEWS_API_BASE_URL"]
+# Getting the source base url
+base_url = app.config["SOURCE_API_BASE_URL"]
+
+
+def get_source():
+    '''
+    Function that gets the json response to our url request
+    '''
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_url_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_results_response['sources']
+            source_results = process_sources(source_results_list)
+
+
+    return source_results
