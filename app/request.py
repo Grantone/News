7f87e6691872506265584e9beb4f3e3535cd1ebd
Granton Mchana <gmchana254@gmail.com>
2017-10-19 15:13:31 +0300

    insertions

--- app/request.py
+++ app/request.py
@@ -1,4 +1,15 @@
 from app import app
+import urllib.request,json
+from .models import article
+from .models import source
+
+Article = article.Article
+Source = source.Source
+
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+
+# Getting the news base url
+base_url = app.config["NEWS_API_BASE_URL"]
