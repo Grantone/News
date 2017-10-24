ca6405a0ad6ece78b9e5c6e51e9569103873c6bf
Granton Mchana <gmchana254@gmail.com>
2017-10-24 21:44:10 +0300

    additions

--- app/models.py
+++ app/models.py
@@ -13,35 +13,35 @@ class Article:
 
 
 
-class Review:
-
-    all_reviews = []
-
-    def __init__(self,article_id,title,imageurl,review):
-        self.article_id = article_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
-
-
-    def save_review(self):
-        Review.all_reviews.append(self)
-
-
-@classmethod
-def clear_reviews(cls):
-    Review.all_reviews.clear()
-
-@classmethod
-def get_reviews(cls,id):
-
-    response = []
-
-    for review in cls.all_reviews:
-        if review.article_id == id:
-                response.append(review)
-
-    return response
+# class Review:
+#
+#     all_reviews = []
+#
+#     def __init__(self,article_id,title,imageurl,review):
+#         self.article_id = article_id
+#         self.title = title
+#         self.imageurl = imageurl
+#         self.review = review
+#
+#
+#     def save_review(self):
+#         Review.all_reviews.append(self)
+#
+#
+# @classmethod
+# def clear_reviews(cls):
+#     Review.all_reviews.clear()
+#
+# @classmethod
+# def get_reviews(cls,id):
+#
+#     response = []
+#
+#     for review in cls.all_reviews:
+#         if review.article_id == id:
+#                 response.append(review)
+#
+#     return response
 
 
 
@@ -60,32 +60,32 @@ class Source:
 
 
 
-class Review:
-
-    all_reviews = []
-
-def __init__(self,source_id,title,imageurl,review):
-    self.source_id = source_id
-    self.title = title
-    self.imageurl = imageurl
-    self.review = review
-
-
-def save_review(self):
-    Review.all_reviews.append(self)
-
-
-@classmethod
-def clear_reviews(cls):
-    Review.all_reviews.clear()
-
-@classmethod
-def get_reviews(cls,id):
-
-    response = []
-
-    for review in cls.all_reviews:
-        if review.source_id == id:
-                response.append(review)
-
-    return response
+# class Review:
+#
+#     all_reviews = []
+#
+# def __init__(self,source_id,title,imageurl,review):
+#     self.source_id = source_id
+#     self.title = title
+#     self.imageurl = imageurl
+#     self.review = review
+#
+#
+# def save_review(self):
+#     Review.all_reviews.append(self)
+#
+#
+# @classmethod
+# def clear_reviews(cls):
+#     Review.all_reviews.clear()
+#
+# @classmethod
+# def get_reviews(cls,id):
+#
+#     response = []
+#
+#     for review in cls.all_reviews:
+#         if review.source_id == id:
+#                 response.append(review)
+#
+#     return response
