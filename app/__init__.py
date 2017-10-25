28ee7c50d1b431688249882aa61194aae40ca6cc
Granton Mchana <gmchana254@gmail.com>
2017-10-25 17:09:19 +0300

    additions

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,14 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
 
-    # Will add the views and forms
+    # Initializing Blueprint
+    from .main import main as main_blueprint
+	app.register_blueprint(main_blueprint)
+
+	from .requests import configure_request
+	configure_request(app)
+
+
     return app
 
  # Registering the blueprint
